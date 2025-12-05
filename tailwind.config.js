28d8d254f7960122f5bbd9a110327d48e7426326
mchrrouhi <mchrrouhi@gmail.com>
2025-12-05 15:13:11 +0100

    ok

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./about.html",
+    "./get-report/index.html",
+    "./blog/**/*.html",
+    "./components/**/*.html",
+  ],
+};
